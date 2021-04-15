f6f9a80498ff4723baec739a1d5ddde2bae2bbf7
Peter <70044794+militarp@users.noreply.github.com>
2021-04-16 08:36:04 +0900

    Update circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -7,9 +7,16 @@ using namespace std;
 int main()
 {
 	double radius;
+	double area;
+	double PI;
+
 	cout << "Enter radius\n";
+	cin >> radius;
+	PI = 3.14159;
+	area = PI * pow(radius, 2);
 	//Add code to read user input and store in variable radius
 	cout << "The area of a circle with radius \n";
+	cout << area;
 	//Calculate the area of the circle
 	return 0;
 }
