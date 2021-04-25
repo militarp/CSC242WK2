1dfd66754e82e1d96a85a6d002da45f50654c794
Peter <70044794+militarp@users.noreply.github.com>
2021-04-25 22:24:28 +0900

    Update circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -3,15 +3,7 @@ using namespace std;
 
 //This program prompts user to enter circle raduis
 //Then the circle area is displayed
-double c(double n)
-{
-	double c;
-	const double PI = 3.14159;
-	
-	c = 2 * PI * n;
-	cout << "Circumference is " << c;
-	return c;
-}
+
 
 int main()
 {
@@ -26,10 +18,6 @@ int main()
 	//Add code to read user input and store in variable radius
 	cout << "The area of a circle with radius \n";
 	cout << area << endl;
-	double h;
-	h = radius;
-	
-	c(h); // Calculates and display the circumference
 	
 	return 0;
 }
